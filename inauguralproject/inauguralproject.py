06cabbe18024e077f58d0b50e68d68f7c283807b
JosefinePedersen <lnr169@alumni.ku.dk>
2023-03-23 19:37:56 +0100

    Kigget på opgave 5

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -52,6 +52,11 @@ class HouseholdSpecializationModelClass:
         sol.beta0 = np.nan
         sol.beta1 = np.nan
 
+        # g. extension to the model - parameter
+        par.k = 0
+
+
+
 
 # Defining the utility function
 
@@ -201,6 +206,43 @@ class HouseholdSpecializationModelClass:
         return sol.beta0,sol.beta1
 
     
+
+    #### QUESTION 5
+
+    # Defining a new utility function with preferences for who in the couple works at home
+
+    def calc_utility5(self,LM,HM,LF,HF):
+        """ calculate utility """
+
+        par = self.par
+        sol = self.sol
+
+        # a. consumption of market goods
+        C = par.wM*LM + par.wF*LF
+
+        # b. home production
+        if par.sigma == 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+        elif par.sigma == 0:
+            H = min(HM,HF)
+        else:
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma) + par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1)) 
+        
+
+        # c. total consumption utility
+        Q = C**par.omega*H**(1-par.omega)
+        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
+
+        # d. disutility of work
+        epsilon_ = 1+1/par.epsilon
+        TM = LM+HM
+        TF = LF+HF
+        disutility = par.nu*(TM**epsilon_/epsilon_+TF**epsilon_/epsilon_)
+
+        # e. preferences for housework
+        pref_H_work = par.k*(HF/HM - HM/HF)
+        
+        return utility - disutility + pref_H_work
     
     def extension(self,alpha=None,sigma=None):
         """ estimate alpha and sigma """
@@ -216,16 +258,92 @@ class HouseholdSpecializationModelClass:
             time_F = LF+HF
             if time_M > 24 or time_F > 24:
                 penalty += 1000 * (max(time_M, time_F) - 24)
-            beta0,beta1 = self.run_regression()
-            self.par.alpha = 0.5
-            return -self.calc_utility(LM,HM,LF,HF) + penalty + (0.4-beta0)**2+(-0.1-beta1)**2
+            return -self.calc_utility5(LM,HM,LF,HF) + penalty
+        
+        # OLD: With methode SLSQP and constraints, but this method could not find the correct results
+        #obj = lambda x: -self.calc_utility(x[0],x[1],x[2],x[3]) # minimize -> negative of utility
+        
+        # b. constraints and bounds
+        #time_constraint_m = lambda x: 24-x[0]-x[1] # violated if negative
+        #time_constraint_f = lambda x: 24-x[2]-x[3] # violated if negative
+        #constraints = [{'type':'ineq','fun':time_constraint_m},{'type':'ineq','fun':time_constraint_f}]
+        #bounds = [(0,24),(0,24),(0,24),(0,24)]
         
         # c. call solve
         x0=[2,2,2,2] # initial guess
         result = optimize.minimize(obj,x0,method='Nelder-Mead')
         
         # d. save results
-    
+        opt.LM = result.x[0]
+        opt.HM = result.x[1]
+        opt.LF = result.x[2]
+        opt.HF = result.x[3]
         #opt.u = self.calc_utility(opt.LM,opt.HM,opt.LF,opt.HF)
         
         return opt
+
+    def solve_wF_vec5(self,discrete=False):
+        """ solve model for vector of female wages """
+
+        par = self.par
+        sol = self.sol
+    
+        for i,wF in enumerate(par.wF_vec):
+            par.wF = wF
+            
+            # Running the model and replacing the values in the vectors with the optimal values
+            opt = self.extension()
+            sol.HF_vec[i]=opt.HF
+            sol.HM_vec[i]=opt.HM
+
+        return sol.HF_vec, sol.HM_vec
+
+
+    def run_regression5(self):
+        """ run regression """
+
+        par = self.par
+        sol = self.sol
+
+        for k in range(-1,1):
+            k = k/100.0    
+            par.k = k
+            
+            self.solve_wF_vec5()
+
+            x = np.log(par.wF_vec)
+            y = np.log(sol.HF_vec/sol.HM_vec)
+            A = np.vstack([np.ones(x.size),x]).T
+            sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0] 
+
+        return sol.beta0,sol.beta1
+    
+
+    def optimalk(self,do_print=False):
+        """ solve model continously """
+
+        par = self.par 
+        sol = self.sol
+        opt = SimpleNamespace()  
+
+        # a. objective function (to minimize)
+        def obj(k):
+            beta0 = 0.4
+            beta1 = -0.1
+            self.solve_wF_vec5()
+            x = np.log(par.wF_vec)
+            y = np.log(sol.HF_vec/sol.HM_vec)
+            A = np.vstack([np.ones(x.size),x]).T
+            par.k = k
+            sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0] 
+            return (beta0-sol.beta0)**2+(beta1-sol.beta1)**2
+        
+        # c. call solve
+        k0=0.0 # initial guess
+        result = optimize.minimize(obj,k0,method='Nelder-Mead')
+        par.k = result.x[0]
+
+        if do_print:
+            print(f"k = {par.k:.4f}")
+        
+        return par.k,sol.beta0,sol.beta1
